381e37aec7fe99e3eb0575c3b17cec94bb26d808
hema <egy.kid1@gmail.com>
2018-02-28 22:59:09 +0200

    first commit

---
+++ hematurnit/Form2.vb
@@ -0,0 +1,18 @@
+Public Class Form2
+
+    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.Hide()
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ThemeButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ThemeButton1.Click
+        Me.Hide()
+    End Sub
+End Class
---
+++ hematurnit/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Hema Turn It")> 
+<Assembly: AssemblyDescription("Turn Off Your Pc After X time")> 
+<Assembly: AssemblyCompany("Hema")> 
+<Assembly: AssemblyProduct("TurnIt")> 
+<Assembly: AssemblyCopyright("Copyright © Hema 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("35354a55-3eb5-4058-8fc6-44f117117772")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
